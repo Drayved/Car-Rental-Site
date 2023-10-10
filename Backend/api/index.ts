57aeaa7679d302c8a4b69ae1aac8223a517dfe82
David Riley <Dril112191@gmail.com>
2023-10-10 10:57:50 -0400

    Update index.ts

--- Backend/api/index.ts
+++ Backend/api/index.ts
@@ -119,6 +119,6 @@ app.post('/users/login', async (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
